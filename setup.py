3affdea53feb92ef7d23ca8b4d9155b8716378aa
Andres Osinski <andres.osinski@gmail.com>
2014-03-26 07:29:49 -0300

    Added distutils files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='AlchemyAPI',
+      version='1.0',
+      description='AlchemyAPI Python bindings',
+      author='AlchemyAPI',
+      url='https://github.com/AlchemyAPI/alchemyapi_python',
+      author_email='support@alchemyapi.com',
+      py_modules=['alchemyapi'])
