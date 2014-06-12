5eaa127647ac863a47ce6cf912507b9b5e59058f
Simon Hewitt <si@sjhewitt.co.uk>
2014-06-12 11:57:05 -0700

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='AlchemyAPI',
-      version='1.0',
+      version='1.1',
       description='AlchemyAPI Python bindings',
       author='AlchemyAPI',
       url='https://github.com/AlchemyAPI/alchemyapi_python',
